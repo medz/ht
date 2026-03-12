c3a644e0ce8958b58bbe9e18a80917e4fd0f6295
Seven Du <5564821+medz@users.noreply.github.com>
2026-03-12 08:02:51 +0800

    refactor(fetch): narrow File parts to BlobPart

--- lib/src/fetch/file.dart
+++ lib/src/fetch/file.dart
@@ -2,9 +2,13 @@ import 'blob.dart';
 
 /// File metadata wrapper over [Blob].
 class File extends Blob {
-  File(Iterable<Object> parts, this.name, {String type = '', int? lastModified})
-    : lastModified = lastModified ?? DateTime.now().millisecondsSinceEpoch,
-      super(parts, type);
+  File(
+    Iterable<BlobPart> parts,
+    this.name, {
+    String type = '',
+    int? lastModified,
+  }) : lastModified = lastModified ?? DateTime.now().millisecondsSinceEpoch,
+       super(parts, type);
 
   final String name;
   final int lastModified;
