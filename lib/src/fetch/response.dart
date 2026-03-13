eeed95e10fa8b661e8b9aa107e898e2a84031093
Seven Du <5564821+medz@users.noreply.github.com>
2026-03-13 12:17:36 +0800

    feat(fetch): add io-backed Response implementation

--- lib/src/fetch/response.dart
+++ lib/src/fetch/response.dart
@@ -1,4 +1,5 @@
 export 'response.native.dart' show ResponseInit, ResponseType;
 export 'response.native.dart'
+    if (dart.library.io) 'response.io.dart'
     if (dart.library.js_interop) 'response.js.dart'
     show Response;
